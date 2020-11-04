f621a4964a37fa7f78635fc3d26879cd0f502a22
Daniel Gomes <dagomes@ua.pt>
2020-11-04 23:30:28 +0000

    More changes in models, i guess

--- webproj/app/models.py
+++ webproj/app/models.py
@@ -1,43 +1,34 @@
 from django.db import models
 from django.db.models import Avg
-
+from django.conf import  settings
+from django.contrib.auth.models import User
 # Create your models here.
 
-class User(models.Model):
-    username=models.CharField(max_length=50,unique=True)
-    password=models.CharField(max_length=50)
-
-class Client(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    email=models.EmailField()
-    fname=models.CharField(max_length=50)
-    lname=models.CharField(max_length=50)
 
 class Developer(models.Model):
     name=models.CharField(max_length=50)
-    #num_apps=models.IntegerField() isto e uma query tb n adianta meter, ia nos foder dps
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    '''
-    @property
-    def average_rating(self):
-        return self.rates.all().aggregate(Avg('rating')).get('rating__avg', 0.00)
-    '''
+
+
 class Category(models.Model):
     title=models.CharField(max_length=50,unique=True)
-
 
 class Product(models.Model):
     name=models.CharField(max_length=50,unique=True)
 
     icon=models.URLField()
-    rating=models.DecimalField(max_digits=2, decimal_places=1)
     description=models.CharField(max_length=50)
     category=models.ManyToManyField(Category)
-    developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    developer = models.ForeignKey(Developer,on_delete=models.CASCADE)
+
+class Client(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    favorites=models.ManyToManyField(Product)
+
 
 class Purchase(models.Model):
     client=models.ForeignKey(Client,on_delete=models.CASCADE)
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
+
 class Prod_Benefits(models.Model):
     title=models.CharField(max_length=50)
     description=models.CharField(max_length=50)
