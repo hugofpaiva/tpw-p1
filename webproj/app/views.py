0946dde0416e206820c1addb0ea1e6b60ff558b3
Daniel Gomes <dagomes@ua.pt>
2020-11-06 22:51:55 +0000

    Sign up feito

--- webproj/app/views.py
+++ webproj/app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-
+from app.forms import *
+from app.models import *
 # Create your views here.
+
 #foi a maneira mais facil q arranjei para saber qual o elemento ativo na navbar, ja que o shop vai extender o base.html(navbars e essas merdas)
 def renderBase(request):
     return render(request, 'base.html',{'activelem': 'home'})
 
 def shopBaseView(request):
+    print(Client.objects.get())
     return render(request,'shop.html',{'activelem': 'shop'})
+
+def register(request):
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            client = Client(user=user)
+            client.save()
+            return render(request,'base.html',{'activelem': 'home'})
+    else:
+        form = SignUpForm()
+    return render(request,'register.html',{'form':form})
--- webproj/webproj/urls.py
+++ webproj/webproj/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.renderBase, name='base'),
+    path('signup',views.register,name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('shop/', views.shopBaseView, name='shop')
