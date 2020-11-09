b5942496a206e5a591b3212a43bd3dffbe53dbbe
hugofpaiva <hugofpaiva@ua.pt>
2020-11-09 13:32:19 +0000

    Fix url

--- webproj/webproj/urls.py
+++ webproj/webproj/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.indexView, name='index'),
     path('signup/',views.register,name='signup'),
-    path('shop/product/<int:idprod>/', views.prodDetails, name='productdetails'),
+    path('shop/product/<int:idprod>', views.prodDetails, name='productdetails'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
